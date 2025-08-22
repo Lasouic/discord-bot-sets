2ebe684c55b07f49bc729ff050879f5d81be7852
Lasouic <119088220+Lasouic@users.noreply.github.com>
2025-08-22 08:06:11 -0700

    1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import {Client, Events, GatewayIntentBits} from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ],
+});
+
+client.once(Events.ClientReady, async(readyClient) => {
+    console.log(`Logged in as ${readyClient.user?.tag}`);
+});
+
+client.on(Events.MessageContent, async(message) => {
+    console.log(`${message.author.tag} said ${message.content}`);
+});
+
+await client.login(process.env.DISCORD_BOT_TOKEN);
