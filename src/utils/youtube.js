3de86a9a0d86ce9afc9e4bfe2332d4dda789df1c
Lasouic <119088220+Lasouic@users.noreply.github.com>
2025-10-04 20:26:22 -0700

    1

--- src/utils/youtube.js
+++ src/utils/youtube.js
@@ -2,7 +2,7 @@ import ytSearch from 'yt-search';
 
 export async function searchYoutube(artist) {
     const result = await ytSearch(`${artist} song`);
-    const videos = result.videos.filter(v => v.url);
+    const videos = result.videos.filter(v => v.seconds < 600);
     if (!videos || videos.length === 0) return null;
     const random = Math.floor(Math.random() * videos.length);
     const video = videos[random];
